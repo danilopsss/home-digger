548762377a0ef91d78f1737ff988fab3465a17a5
Danilo Paes <danilo.paes@pm.me>
2023-12-13 17:23:30 +0100

    chore: moving files to app dir

--- main.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from homedigger.core.scrapper import Scrapper
-
-
-if __name__ == "__main__":
-    files = Path(__file__).parent.joinpath("html").rglob("*.html")
-    for file in files:
-        scrapper = Scrapper(file)
-        print(scrapper.run().model_dump_json())
